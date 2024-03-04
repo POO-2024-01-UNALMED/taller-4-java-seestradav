5462d706d18c397c87ce192b3838dbadf390827f
seestradav <seestradav@unal.edu.co>
2024-03-03 23:43:10 -0500

    PARTE 1

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -3,10 +3,8 @@ package classroom;
 public enum Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
-	private int codigo;
-	private String nombre;
-	public Tipo(int codigo,String nombre) {
-		this.codigo = codigo;
-		this.nombre = nombre;
+	final int codigo;
+	private Tipo(int num) {
+		this.codigo = num;
 	}
 }
